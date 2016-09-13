d01373c08ec2833dba256b807c40551d0ac5fc53
Hady Rashwan <h2rashwan@gmail.com>
2016-09-13 12:54:48 +0200

    return del
    he did it my way he used diff syntax will say later why

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,49 +6,58 @@
  */
 
 module.exports = {
-	"new":function(req,res){
-		res.view();
-	},
-	create:function(req,res,next){
-		Customer.create(req.params.all(),function customerCreated(err,customer){
-			if(err) return next(err);
-			//res.json(customer);
-			res.redirect("/customer/show/"+customer.id);
-		});
-	},
-	show:function(req,res,next){
-		Customer.findOne(req.param('id'),function foundCustomer(err , customer){
-			if(err) return next(err);
-			if(!customer) return next();
-			res.view({
-				customer:customer
-			});
-		});
-	},
-	index:function(req,res,next){
-		Customer.find( function foundCustomers(err,customers){
-			if(err) return next(err);
-			if(!customers) return next();
-			res.view({
-				customers:customers
-			});
-		});
-	},
-	edit:function(req,res,next){
-		Customer.findOne(req.param('id'),function foundCustomer(err,customer){
-			if(err) return next(err);
-			if(!customer) return next();
-			res.view({
-				customer:customer
-			});
-		});
-	},
-	update:function(req,res,next){
-		Customer.update(req.param('id'),req.params.all(),function customerUpdate(err,customer){
-			if(err){
-				return res.redirect('/customer/edit/'+req.param('id'));
-			}
-			res.redirect('/customer/show/'+ req.param('id'));
-		});
-	}
+  "new": function(req, res) {
+    res.view();
+  },
+  create: function(req, res, next) {
+    Customer.create(req.params.all(), function customerCreated(err, customer) {
+      if (err) return next(err);
+      //res.json(customer);
+      res.redirect("/customer/show/" + customer.id);
+    });
+  },
+  show: function(req, res, next) {
+    Customer.findOne(req.param('id'), function foundCustomer(err, customer) {
+      if (err) return next(err);
+      if (!customer) return next();
+      res.view({
+        customer: customer
+      });
+    });
+  },
+  index: function(req, res, next) {
+    Customer.find(function foundCustomers(err, customers) {
+      if (err) return next(err);
+      if (!customers) return next();
+      res.view({
+        customers: customers
+      });
+    });
+  },
+  edit: function(req, res, next) {
+    Customer.findOne(req.param('id'), function foundCustomer(err, customer) {
+      if (err) return next(err);
+      if (!customer) return next();
+      res.view({
+        customer: customer
+      });
+    });
+  },
+  update: function(req, res, next) {
+    Customer.update(req.param('id'), req.params.all(), function customerUpdate(err, customer) {
+      if (err) {
+        return res.redirect('/customer/edit/' + req.param('id'));
+      }
+      res.redirect('/customer/show/' + req.param('id'));
+    });
+  },
+  destroy: function(req, res, next) {
+    Customer.destroy(req.param('id')).exec(function(err, customer) {
+      if (err) {
+        res.redirect('/customer');
+      }
+      res.redirect('/customer');
+
+    });
+  }
 };
